266c285afaa67e25e75d0619b2607476463a4a0f
Dmitry <Shimansky.Dmitry@gmail.com>
2023-10-19 22:14:10 +0300

    add: Sliders

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
+const headerButton = document.querySelector(".header-button");
 
 const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
@@ -33,4 +34,69 @@ window.addEventListener("scroll", () => {
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
   menu.classList.contains("is-open") ? closeMenu() : openMenu();
+});
+
+// Header slider
+const swiper = new Swiper("#swiper1", {
+  // Optional parameters
+  speed: 400,
+  autoHeight: true,
+  direction: "horizontal",
+  slidesPerView: 1,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+// Second slider
+const secondSwiper = new Swiper("#swiper2", {
+  // Optional parameters
+  speed: 400,
+  autoHeight: false,
+  direction: "horizontal",
+  slidesPerView: 1,
+  slidesOffsetAfter: -50,
+  slidesOffsetBefore: 30,
+  navigation: {
+    nextEl: ".second-slider-button-next",
+    prevEl: ".second-slider-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 5,
+    },
+  },
 });
