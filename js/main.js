6a300cccd56b9769d070b099f643a0ed2b429599
Dmitry <Shimansky.Dmitry@gmail.com>
2023-11-09 17:11:16 +0200

    add: functional for open/close modal window

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const logo = document.querySelector(".logo");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 const headerButton = document.querySelector(".header-button");
+const modal = document.querySelector(".modal");
+const modalDialog = document.querySelector(".modal-dialog");
 
 const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
@@ -126,17 +128,20 @@ const swiperBlog = new Swiper(".blog-slider", {
   },
 });
 
-const modal = document.querySelector(".modal");
-const modalToggle = document.querySelectorAll("[data-toggle=modal]");
-const modalClose = document.querySelector(".modal-close");
-
-modalToggle.forEach((element) => {
-  element.addEventListener("click", (event) => {
+//Modal window open/close
+document.addEventListener("click", (event) => {
+  if (
+    event.target.dataset.toggle == "modal" ||
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) &&
+      modal.classList.contains("is-open"))
+  ) {
     event.preventDefault();
-    modal.classList.add("is-open");
-  });
+    modal.classList.toggle("is-open");
+  }
 });
-modalClose.addEventListener("click", (event) => {
-  event.preventDefault();
-  modal.classList.remove("is-open");
+document.addEventListener("keyup", (event) => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    modal.classList.toggle("is-open");
+  }
 });
